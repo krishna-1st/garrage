1acbe43bb8b67c5574325588ccae6bcadb07d644
krishna-1st <krishnabhattarai189@gmail.com>
2023-12-28 10:25:18 +0545

    done with changes

--- garrage/settings.py
+++ garrage/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0qo+$t1(vw)r-v#$+-*1hcg7vi3y3iv=ovv^31071!c_-qt^m#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
